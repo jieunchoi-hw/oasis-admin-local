74aa27a84ef863da96ab1d2f1181c6d8e6d7e7ce
jieunchoi-hw <140673391+jieunchoi-hw@users.noreply.github.com>
2025-07-25 08:05:39 +0000

    Deploying to gh-pages from @ jieunchoi-hw/oasis-admin-local@738ab7ae0ae8e8bbb06f401e1690cdc3993595f5 🚀

--- assets/ssrBoot-BqSB6x5u.js
+++
@@ -1,1 +0,0 @@
-import{A as t,l as e,g as s,x as n}from"./index-goOt96qe.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
---
+++ assets/tenant-C2GrbkCs.js
@@ -0,0 +1,1 @@
+import{an as n}from"./index-BD_oSQYS.js";const a=n("tenant",{state:()=>({tenantId:1}),actions:{setTenantId(t){this.tenantId=t}}});export{a as u};
