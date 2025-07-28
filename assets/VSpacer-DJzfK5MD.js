780a662160c66e6975d32248fe20da57bea0efcb
jieunchoi-hw <jieunchoi-hw@users.noreply.github.com>
2025-07-28 05:38:29 +0000

    deploy: 478a5be92bdabf255272dd137ae4a9962a0d22e8

---
+++ assets/VSpacer-DJzfK5MD.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as r}from"./createSimpleFunctional-DD5VFrvS.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ assets/tenant-BQbMv5ge.js
@@ -0,0 +1,1 @@
+import{ac as e}from"./index-mzeqWVVh.js";const a=e("tenant",{state:()=>({tenantId:1}),actions:{setTenantId(t){this.tenantId=t}}});export{a as u};
