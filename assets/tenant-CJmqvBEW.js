b086818db8153f0285b0b452ab2434af8cbf5bee
jieunchoi-hw <jieunchoi-hw@users.noreply.github.com>
2025-07-28 01:40:26 +0000

    deploy: 17ef1371d8fd55e7a3aeb852580e1175026eec1f

---
+++ assets/tenant-CJmqvBEW.js
@@ -0,0 +1,1 @@
+import{ao as e}from"./index-DiedJyfL.js";const a=e("tenant",{state:()=>({tenantId:1}),actions:{setTenantId(t){this.tenantId=t}}});export{a as u};
--- assets/tenant-DsANhIQ9.js
+++
@@ -1,1 +0,0 @@
-import{ao as e}from"./index-DQnrQP0W.js";const a=e("tenant",{state:()=>({tenantId:1}),actions:{setTenantId(t){this.tenantId=t}}});export{a as u};
